6b9bd00cfa2b9a759d96c8d983ad72184b896c2c
Vitaliy <vitaliypolyanskiy@gmail.com>
2024-11-18 16:46:15 +0200

    Add project files.

---
+++ 1.DisposeObject/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.IO;
+
+namespace DisposeObject
+{
+    // Данный класс реализует интерфейс IDisposable
+    class FinalizeObject : IDisposable
+    {
+        FileStream file;
+        BinaryWriter writer;
+        public int id { get; set; }
+
+        public FinalizeObject(int id)
+        {
+            this.id = id;
+            file = new FileStream(id + ".dat", FileMode.Create, FileAccess.Write);
+            writer = new BinaryWriter(file);
+            writer.Write("IDisposable");
+        }
+
+        // Реализуем метод Dispose()
+        public void Dispose()
+        {
+            Console.WriteLine("Освобождение ресурсов объекта!");
+            writer.Close();
+            file.Close();
+            // Освобождаем управляемые и неуправляемые ресурсы
+        }
+    }
+
+    // В .NET существует два типа ресурсов: управляемые и неуправляемые. 
+    // К неуправляемым ресурсам относятся только «сырые» ресурсы, типа IntPtr, дескрипторы сокетов или файлов.
+    // Если же этот ресурс упаковали в объект, захватывающий его в конструкторе и освобождающий в методе Dispose, 
+    // то такой ресурс уже является управляемым. 
+    // По сути, управляемые ресурсы – это «умные оболочки» для неуправляемых ресурсов, для освобождения которых 
+    // не нужно вызывать какие-то функции, а достаточно вызвать метод Dispose интерфейса IDisposable
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FinalizeObject[] obj = new FinalizeObject[30];
+            for (int i = 0; i < 30; i++)
+                obj[i] = new FinalizeObject(i);
+            for (int i = 0; i < 30; i++)
+                obj[i].Dispose();
+
+            Console.Read();
+        }
+    }
+}
---
+++ 2.DisposeObjectByUsing/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.IO;
+
+namespace DisposeObjectByUsing
+{
+    // Данный класс реализует интерейс IDisposable
+    class FinalizeObject : IDisposable
+    {
+        public int id { get; set; }
+
+        FileStream file;
+        BinaryWriter writer;
+        public FinalizeObject(int id)
+        {
+            this.id = id;
+            file = new FileStream(id + ".dat", FileMode.Create, FileAccess.Write);
+            writer = new BinaryWriter(file);
+            writer.Write("IDisposable");
+        }
+
+        // Реализуем метод Dispose()
+        public void Dispose()
+        {
+            Console.WriteLine("Освобождение ресурсов объекта!");
+            writer.Close();
+            file.Close();
+            // Освобождаем управляемые и неуправляемые ресурсы
+        }
+    }
+
+    // В .NET существует два типа ресурсов: управляемые и неуправляемые. 
+    // К неуправляемым ресурсам относятся только «сырые» ресурсы, типа IntPtr, дескрипторы сокетов или файлов.
+    // Если же этот ресурс упаковали в объект, захватывающий его в конструкторе и освобождающий в методе Dispose, 
+    // то такой ресурс уже является управляемым. 
+    // По сути, управляемые ресурсы – это «умные оболочки» для неуправляемых ресурсов, для освобождения которых 
+    // не нужно вызывать какие-то функции, а достаточно вызвать метод Dispose интерфейса IDisposable
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FinalizeObject[] obj = new FinalizeObject[30];
+            for (int i = 0; i < 30; i++)
+                using (obj[i] = new FinalizeObject(i))
+                {
+                    // Выполнение действий на объектами
+                }
+
+            Console.Read();
+        }
+    }
+}
